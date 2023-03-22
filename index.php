c407b1660cf45e645ce1908fb19b8cbea2977ce0
yuruhenro <128557320+yuruhenro@users.noreply.github.com>
2023-03-22 15:32:40 +0900

    Update index.php
    destyle/cssを/assets/css/destyle.cssに書き換えました。
    
    藤

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name') ?></title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/destyle.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
