155116e39eb4d9d56cada6d93b71cedd57ea284e
Shinomiya-Aki <shinomiya.aki.22@gmail.com>
2023-03-26 09:02:16 +0900

    common_script.js修正
    common_script.jsのclassタグ修正による記述修正
    
    四宮

--- assets/js/common_script.js
+++ assets/js/common_script.js
@@ -7,14 +7,14 @@
 //--------------------------------------------------//
 //ハンバーガーメニューが押されたら//
 $(".header_hamburger").on("click", function () {
-    //.header_hamburgerと.header_gnav.activeを付与
+    //.header_hamburgerと.header_nav.activeを付与
     $(".header_hamburger").toggleClass("active");
-    $(".header_gnav").toggleClass("active");
+    $(".header_nav").toggleClass("active");
 });
 //リンク先が押されたら//
 $(".nav_menu a").click(function () {
-    //.header_hamburgerと.header_gnavから.activeを削除してメニューを閉じる
+    //.header_hamburgerと.header_navから.activeを削除してメニューを閉じる
     $(".header_hamburger").removeClass("active");
-    $(".header_gnav").removeClass("active");
+    $(".header_nav").removeClass("active");
 });
 //--------------------------------------------------//
