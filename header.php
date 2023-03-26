f372b27aaa66292c4b9ca85b3b5ae049f779cb29
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-03-27 06:08:18 +0900

    php編集
    footer header index single-temple.phpの編集
    中西

---
+++ header.php
@@ -0,0 +1,61 @@
+<!--headerとfooterのベース作成用-->
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>タイトル</title>
+    <!-- CSS読み込み -->
+    <!-- リセットCSS -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css" />
+    <!-- Googleフォント Kosugi Maru,Kiwi Maru -->
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300;400;500&family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <!-- Instagramアイコンフォント(font awesome) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- js用CSS -->
+
+    <!-- オリジナルCSS -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/common.css" />
+    <!-- jQuery読み込み -->
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.6.3.min.js"></script>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <div class="wrap">
+        <!-- headerここから -->
+        <header>
+            <div class="header_inner flex">
+                <div class="header_logo">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/header_logo_01.png" alt="とくしま１日＃ゆるへんろロゴ">
+                    </a>
+                </div>
+
+                <div class="header_menu">
+                    <!-- ハンバーガーボタン -->
+                    <div class="header_hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+                    <!-- ナビゲーションメニュー -->
+                    <nav class="header_nav flex">
+                        <ul>
+                            <li><a href="<?php echo get_template_directory_uri(); ?>/page">tstゆるへんろって？</a></li>
+                            <li><a href="#">参拝スタイル</a></li>
+                            <li><a href="#">コース紹介</a></li>
+                            <li><a href="#">マイブックマーク</a></li>
+                            <li><a href="#">検索</a></li>
+                            <li><a href="#">Q&A</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </header>
+        <!-- headerここまで -->
--- index.php
+++ index.php
@@ -1,91 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name') ?></title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=Noto+Sans+JP:wght@300;400;700;900&family=PT+Serif:ital@1&family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
+<?php get_header(); ?>
+<!-- mainここから -->
+<main>
+    <div class="main_inner">
 
-</head>
-<body>
-    <header class="header">
-        <div class="logo">
-            <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/ゆるへんろ ロゴ.png" alt=""></a>
-        </div>
-        <nav>
-            <ul class="gnav">
-                <li><a href="<?php echo get_template_directory_uri(); ?>/about.html">#ゆるへんろとは</a></li>
-                <li><a href="<?php echo get_template_directory_uri(); ?>/course.html">入門コース</a></li>
-                <li><a href="<?php echo get_template_directory_uri(); ?>/course.html">チャレンジコース</a></li>
-                <li><a href="<?php echo get_template_directory_uri(); ?>/bookmark.html">マイブックマーク</a></li>
-                <li><a href="<?php echo get_template_directory_uri(); ?>/qanda.html">Q&A</a></li>
-            </ul>
-        </nav>
-    </header>
-    <div class="kv">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/kv.png" alt="">
-        <p>どこ行くん？<br>&nbsp;&nbsp;ゆるっと遍路<br>&nbsp;&nbsp;#ゆるへんろ</p>
     </div>
-    <div class="insta">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/ロゴ.png" alt="">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/ロゴ.png" alt="">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/ロゴ.png" alt="">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/ロゴ.png" alt="">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/ロゴ.png" alt="">
-    </div>
-    <main class="main">
-        <section class="sec sec1">
-            <div class="about1">
-                <h2 class="title">#ゆるへんろって？</h2>
-                <div class="abouttext">
-                    「おへんろ」ってどうやって始めたらいいの？<br>
-                    やってみたいけど難しいんじゃない？<br>
-                    あと一歩が踏み出せないあなたへのおへんろのススメ！
-                </div>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/tokushimamap.png" alt="">
-            </div>
-        </section>
-        <section class="sec sec2">
-            <div class="about2">
-                <h2 class="title">おへんろ知っとこ！</h2>
-                <div class="abouttext">
-                    気軽におへんろに行ってほしい。<br>
-                    だけどちゃんと押さえる所は押さえとこ！<br>
-                    大丈夫、簡単だから！
-                </div>
-            </div>
-        </section>
-        <section class="sec sec3">
-            <div class="about3">作法について&nbsp;&nbsp; <a href="checkpoint.html">参拝スタイル</a></span>
-        </section>
-        <section class="sec sec4">
-            <div class="about1">
-                <h2 class="title">コース紹介</h2>
-                <div class="abouttext">
-                    おすすめしたい入門コース<br>
-                    ３つのお寺をめぐって<br>
-                    おへんろの楽しさを感じてほしい！<br>
-                    <p><a href="">#ゆるへんろ&nbsp;入門コース＞＞＞</a></p>
-                    入門コースじゃものたりない！<br>
-                    そんなあなたにはチャレンジコース<br>
-                    徳島の地域ごとにコースをご提案します！<br>
-                    <p><a href="">#ゆるへんろ&nbsp;チャレンジコース！</a></p>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/tokushimamap.png" alt="">
-                </div>
-            </div>
-        </section>
-        <div class="foot">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/footimg.png" alt="">
-        </div>
-    </main>
-    <footer class="footer">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/ゆるへんろ ロゴ.png" alt="">
-        <small>©とくしま1日#ゆるへんろtest</small>
-    </footer>
-</body>
-</html>
+</main>
+<!-- mainここまで -->
+<?php get_footer(); ?>
