db20dcecbae536d7172e0dafb7c71f7449ab0d85
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-03-28 13:28:16 +0900

    php編集中
    jq3.6.3に修正
    
    中西

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!is_admin()) {
     wp_deregister_script('jquery');
 
         //GoogleCDNから読み込む
-    wp_enqueue_script('jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js' );
+    wp_enqueue_script('jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js' );
         }
 }
 add_action('wp_print_scripts', 'custom_print_scripts');?>
