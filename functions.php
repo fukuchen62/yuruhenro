8d3c186bde3c36e9b79b13486e72d72014383c0b
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-03-28 15:52:47 +0900

    php編集中
    functions周り
    
    中西

--- functions.php
+++ functions.php
@@ -1,18 +1,57 @@
 <?php
-function add_my_styles() {
-if (is_single()) {
-wp_enqueue_style( 'single-course', get_template_directory_uri() . '/assets/css/course.css' );
+//after_setup_themeアクションフックを使用する関数をまとめる
+add_action( 'after_setup_theme', 'my_theme_setup' );
+function my_theme_setup() {
+    add_theme_support( 'title-tag' );//<title>タグを出力する
+    add_theme_support( 'post-thumbnails' );//アイキャッチ画像を使用可能にする
+    add_theme_support('menus');//カスタムメニュー機能を使用可能にする
 }
+// functions.phpでスタイルシートとJavaScriptファイルを読み込む
+add_action('wp_enqueue_scripts', 'add_my_files');
+function add_my_files() {
+    //jquery読み込み
+    wp_enqueue_script('jquery');
+    //以下はheaderに出力
+    wp_enqueue_style(
+        'google-fonts-kosugi',
+        'https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300;400;500&family=Kosugi+Maru&display=swap'
+    );
+    wp_enqueue_style(
+        'instagram-icon-photo-font-awesome',
+        'https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300;400;500&family=Kosugi+Maru&display=swap'
+    );
+    wp_enqueue_style(
+        'my-destyle',
+        get_template_directory_uri() . '/assets/css/destyle.css'
+    );
+    wp_enqueue_style(
+        'my-common',
+        get_template_directory_uri() . '/assets/css/common.css'
+    );
+    wp_enqueue_style(
+        'my-course',
+        get_template_directory_uri() . '/assets/css/course.css'
+    );
+    wp_enqueue_style(
+        'my-temple',
+        get_template_directory_uri() . '/assets/css/temple.css'
+    );
+    wp_enqueue_script(
+        'jquery-3.6.3',
+        get_template_directory_uri() . '/assets/js/jquery-3.6.3.min.js'
+    );
+    wp_enqueue_script(
+        'common_script',
+        get_template_directory_uri() . '/assets/js/common_script.js',
+        array(),
+        false,
+        true
+    );
+    wp_enqueue_script(
+        'course_script',
+        get_template_directory_uri() . '/assets/js/course_script.js',
+        array(),
+        false,
+        true
+    );
 }
-add_action( 'wp_enqueue_scripts', 'add_my_styles' );
-
-function custom_print_scripts() {
-if (!is_admin()) {
-        //デフォルトjquery削除
-    wp_deregister_script('jquery');
-
-        //GoogleCDNから読み込む
-    wp_enqueue_script('jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js' );
-        }
-}
-add_action('wp_print_scripts', 'custom_print_scripts');?>
--- header.php
+++ header.php
@@ -18,14 +18,11 @@
     <!-- js用CSS -->
 
     <!-- オリジナルCSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/common.css" />
+
     <!-- コース用CSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/course.css" />
+
     <!-- jQuery読み込み -->
-    <?php
-// wp_deregister_script('jquery');
-// wp_enqueue_script('jquery','https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js',array(),'3.6.3');
-?>
+
     <?php wp_head(); ?>
 </head>
 
