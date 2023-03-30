cdd5b0bd0b166a2e806dc27fca980a5b1c8b33a6
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-03-30 16:29:13 +0900

    php編集中
    編集中
    中西

--- search.php
+++ search.php
@@ -1,6 +1,23 @@
 <?php
-/*Template Name:
+/*Template Name: search
 */
 ?>
 <?php get_header(); ?>
+<!-- mainここから -->
+<main>
+    <div class="main_inner">
+        <!-- h1 -->
+        <div class="h1_bg">
+            <h1>条件検索</h1>
+        </div>
+        <section class="sec_mb40">
+            <h2 class="h2_bg">条件検索結果</h2>
+            <!-- ブックマーク -->
+            <div class="contents_width display_area">
+                <p>検索結果はありませんでした。</p>
+            </div>
+        </section>
+    </div>
+</main>
+<!-- mainここまで -->
 <?php get_footer(); ?>
