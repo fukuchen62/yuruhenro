52953f5a24f52ae8e5cd6ff319ea6aee0666cec8
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-04 18:49:42 +0900

    Update page-bookmark.php
    寺社と周辺施設入れ替えました（特に問題なさそう
    
    中西

--- page-bookmark.php
+++ page-bookmark.php
@@ -7,52 +7,6 @@
 <main>
     <div class="main_inner">
         <h1 class="h1_bg">マイブックマーク</h1>
-        <h2 class="h2_bg">周辺施設お気に入り</h2>
-        <div class="shop_info_list">
-            <?php
-$favorites = get_user_favorites();
-if ($favorites) :
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$favorites_query = new WP_Query(array(
-'post_type' => 'facility',
-'posts_per_page' => -1,
-'ignore_sticky_posts' => true,
-'post__in' => $favorites,
-'paged' => $paged,
-));
-if ($favorites_query->have_posts()) : while ($favorites_query->have_posts()) : $favorites_query->the_post();
-?>
-
-            <div class="shop_info_card mb_40">
-                <div class="shop_info_caption">
-                    <img src="<?php the_field('f_pic1'); ?>" alt="各施設の画像" />
-                </div>
-                <div class="shop_info_title">
-                    <p><?php the_field('f_name'); ?></p>
-                </div>
-                <div class="shop_info_text">
-                    <p>営業時間：<?php the_field('hours'); ?></p>
-                    <p>休業日：<?php the_field('holiday'); ?></p>
-                    <p>駐車場：<?php the_field('f_parking'); ?></p>
-                    <p>TEL：<?php the_field('f_tell'); ?></p>
-                    <p>公式HP：<?php the_field('f_url'); ?></p>
-                </div>
-                <p class="shop_info_article">
-                                                <?php the_field('f_message'); ?>
-                                            </p>
-
-            </div>
-            <?php endwhile ?>
-            <?php endif;
-        wp_reset_postdata(); ?>
-            <?php else : ?>
-            <p>お気に入りがありません。</p>
-            <?php endif ?>
-
-
-        </div>
-
-
         <h2 class="h2_bg">寺社お気に入り</h2>
 
         <div class="shop_info_list">
@@ -90,6 +44,51 @@ if ($temple_favorites_query->have_posts()) : while ($temple_favorites_query->hav
         <?php else : ?>
         <p class="text-center">お気に入りがありません。</p>
         <?php endif ?>
+    </div>
+
+    <h2 class="h2_bg">周辺施設お気に入り</h2>
+    <div class="shop_info_list">
+        <?php
+$favorites = get_user_favorites();
+if ($favorites) :
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$favorites_query = new WP_Query(array(
+'post_type' => 'facility',
+'posts_per_page' => -1,
+'ignore_sticky_posts' => true,
+'post__in' => $favorites,
+'paged' => $paged,
+));
+if ($favorites_query->have_posts()) : while ($favorites_query->have_posts()) : $favorites_query->the_post();
+?>
+
+        <div class="shop_info_card mb_40">
+            <div class="shop_info_caption">
+                <img src="<?php the_field('f_pic1'); ?>" alt="各施設の画像" />
+            </div>
+            <div class="shop_info_title">
+                <p><?php the_field('f_name'); ?></p>
+            </div>
+            <div class="shop_info_text">
+                <p>営業時間：<?php the_field('hours'); ?></p>
+                <p>休業日：<?php the_field('holiday'); ?></p>
+                <p>駐車場：<?php the_field('f_parking'); ?></p>
+                <p>TEL：<?php the_field('f_tell'); ?></p>
+                <p>公式HP：<?php the_field('f_url'); ?></p>
+            </div>
+            <p class="shop_info_article">
+                                                <?php the_field('f_message'); ?>
+                                            </p>
+
+        </div>
+        <?php endwhile ?>
+        <?php endif;
+        wp_reset_postdata(); ?>
+        <?php else : ?>
+        <p>お気に入りがありません。</p>
+        <?php endif ?>
+
+
     </div>
 
 
