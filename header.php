cc6abe4f4f84c39f0095018a60e12da8a57fd0fc
yuruhenro <128557320+yuruhenro@users.noreply.github.com>
2023-04-05 14:42:59 +0900

    変更色々
    functions.php タブのタイトルを表示させるコードを追加
    header.php タイトルを削除
    search-form.php 検索条件ページ作業中

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>タイトル</title>
     <!-- CSS読み込み -->
     <!-- リセットCSS -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css" />
--- search-form.php
+++ search-form.php
@@ -11,30 +11,32 @@
             <p class="big_cap">該当する条件で検索することができます。<br>条件を選択後、「検索」ボタンを押してください。</p>
         </section>
         <section class="sec_mb40 search_form">
-            <form method="post" action="   ">
+            <form method="get" action="/">
                 <h3 class="h3_bg_ex">検索条件選択</h3><br>
                 <div class="search-form_checkarea">
                     <p class="search-form_headline">エリア</p>
                     <div class="searchkinds  mb_40">
-                        <div class="checkbox"><input type="checkbox" name="q1" value="初心者"> 初心者</div>
-                        <div class="checkbox"><input type="checkbox" name="q1" value="県北"> 県北</div>
-                        <div class="checkbox"><input type="checkbox" name="q1" value="県西"> 県西</div>
-                        <div class="checkbox"><input type="checkbox" name="q1" value="県南"> 県南</div>
-                        <div class="checkbox"><input type="checkbox" name="q1" value="市街地"> 市街地</div>
+                        <div class="checkbox"><input type="checkbox" name="area[]" value="初心者"> 初心者</div>
+                        <div class="checkbox"><input type="checkbox" name="area[]" value="県北"> 県北</div>
+                        <div class="checkbox"><input type="checkbox" name="area[]" value="県西"> 県西</div>
+                        <div class="checkbox"><input type="checkbox" name="area[]" value="県南"> 県南</div>
+                        <div class="checkbox"><input type="checkbox" name="area[]" value="市街地"> 市街地</div>
                     </div>
 
                     <p class="search-form_headline">寺院内設備</p>
                     <div class="searchkinds">
-                        <div class="checkbox"><input type="checkbox" name="q2" value="無料駐車場"> 無料駐車場</div>
-                        <div class="checkbox"><input type="checkbox" name="q2" value="食事"> 食事</div>
-                        <div class="checkbox"><input type="checkbox" name="q2" value="宿坊"> 宿坊</div>
-                        <div class="checkbox"><input type="checkbox" name="q2" value="売店"> 売店</div>
-                        <div class="checkbox"><input type="checkbox" name="q2" value="鐘撞き可"> 鐘撞き可</div>
+                        <div class="checkbox"><input type="checkbox" name="searchh[]" value="無料駐車場"> 無料駐車場</div>
+                        <div class="checkbox"><input type="checkbox" name="searchh[]" value="食事"> 食事</div>
+                        <div class="checkbox"><input type="checkbox" name="searchh[]" value="宿坊"> 宿坊</div>
+                        <div class="checkbox"><input type="checkbox" name="searchh[]" value="売店"> 売店</div>
+                        <div class="checkbox"><input type="checkbox" name="searchh[]" value="鐘撞き可"> 鐘撞き可</div>
                     </div>
                     <p class="searchBtnsArea"><input class="searchBtns" type="submit" value="検索">
             <input  class="searchBtns" type="reset" name="reset" value="リセット"></p>
             </form>
         </section>
+
+
         <!-- サービス紹介 -->
         <section class="sec_mb40">
             <div class="searchresult_inner">
