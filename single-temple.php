ac79693857da8eb4ab94c69ad1fc7dd2593c39c7
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-05 11:29:48 +0900

    php編集中
    周辺施設表示をランダムに変更
    
    中西

--- single-temple.php
+++ single-temple.php
@@ -89,17 +89,28 @@
                 <h2 class="h3_bg">周辺施設</h2>
                 <div class="shop_info_list">
                     <?php
-$taxonomy_slug = "area";
-$taxonomy = get_the_terms($post->ID,$taxonomy_slug);
-// print_r($taxonomy[0]->slug);
+$terms = get_the_terms($post->ID,'area');
+foreach( $terms as $term ) {
+    $term_slug = $term->slug; // 現在表示している投稿が所属しているタームを取得
+}
 $facility_args = array(
-'post_type' => 'facility',
-'posts_per_page' => 3,
-'taxonomy' => $taxonomy[0]->slug,
-);
-$facility_query = new WP_Query( $facility_args );
-        if ( $facility_query->have_posts() ) :
-        while ( $facility_query->have_posts() ) : $facility_query->the_post(); ?>
+    'post_type' => 'facility', // 投稿タイプ名
+    'post__not_in' => array($post->ID), // 現在表示している投稿を除外
+    'posts_per_page' => 3, // 表示件数
+    'orderby' => 'rand', // ランダム表示
+    'tax_query' => array(
+    array(
+      'taxonomy' => 'area', // タクソノミー名
+    'field' => 'slug',
+      'terms' => $term_slug, // 取得したタームを指定
+    )
+    )
+); $the_query = new WP_Query($facility_args); if($the_query->have_posts()):
+
+                    $facility_query = new WP_Query( $facility_args );
+                    if ( $facility_query->have_posts() ) :
+                    while ( $facility_query->have_posts() ) : $facility_query->the_post(); ?>
+
 
                     <div class="shop_info_card mb_40">
                         <div class="shop_info_caption">
@@ -120,46 +131,9 @@ $facility_query = new WP_Query( $facility_args );
                     </div>
                     <?php endwhile;?>
                     <?php endif;?>
+                    <?php endif;?>
+
                     <?php wp_reset_postdata(); ?>
-
-
-                    <!-- <div class="shop_info_card">
-                        <div class="shop_info_caption">
-                            <img src="" alt="各施設の画像" />
-                        </div>
-                        <div class="shop_info_title">
-                            <p>タイトル(施設名)</p>
-                        </div>
-                        <div class="shop_info_text">
-                            <p>営業時間：10時～17時</p>
-                            <p>休業日：火曜日</p>
-                            <p>駐車場：あり</p>
-                            <p>TEL：000-000-0000</p>
-                            <p>公式HP：example.site</p>
-                        </div>
-                        <p class="shop_info_article">
-                    １２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０
-                </p>
-                    </div>
-
-                    <div class="shop_info_card mb_40">
-                        <div class="shop_info_caption">
-                            <img src="" alt="各施設の画像" />
-                        </div>
-                        <div class="shop_info_title">
-                            <p>タイトル(施設名)</p>
-                        </div>
-                        <div class="shop_info_text">
-                            <p>営業時間：10時～17時</p>
-                            <p>休業日：火曜日</p>
-                            <p>駐車場：あり</p>
-                            <p>TEL：000-000-0000</p>
-                            <p>公式HP：example.site</p>
-                        </div>
-                        <p class="shop_info_article">
-                    １２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０
-                </p>
-                    </div> -->
                 </div>
             </section>
 
