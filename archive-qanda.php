9a057c134adf0183b00c679632df5d3f6554c28d
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-06 09:23:26 +0900

    Update archive-qanda.php
    Q&AのQに番号が振られるように修正しました
    
    中西

--- archive-qanda.php
+++ archive-qanda.php
@@ -15,31 +15,35 @@
         <h2 class="h2_bg mb_40">よくあるご質問</h2>
         <div class="contents_width">
             <p class="big_cap">
-                            よくあるご質問をまとめています。おへんろ活動のご参考にどうぞ。
-                        </p>
+                よくあるご質問をまとめています。おへんろ活動のご参考にどうぞ。
+            </p>
 
 
             <!-- 以下、Q&A開始 -->
             <section class=" mb_40 js-fadeUp">
                 <?php $args = array(
-        'post_type' => 'qanda',
-        'posts_per_page' => -1 //表示件数（-1で全ての記事を表示
-        );
-        $the_query = new WP_Query( $args );
-        if ( $the_query->have_posts() ) :
-        while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                <div class="section_line">
-                    <div class="rowLine flex">
-                        <span class="circle_mark1">Q</span>
-                        <p><?php the_title();?></p>
-                    </div>
-                    <div class="rowLine flex">
-                        <span class=" circle_mark2">A</span>
-                        <p><?php the_field('answer');?></p>
-                    </div>
-                </div>
-                <?php endwhile;?>
-                <?php endif;?>
+                    'post_type' => 'qanda',
+                    'posts_per_page' => -1 //表示件数（-1で全ての記事を表示
+                );
+                $post_counter = 1;
+
+                $the_query = new WP_Query($args);
+                if ($the_query->have_posts()) :
+                    while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                        <div class="section_line">
+                            <div class="rowLine flex">
+                                <span class="circle_mark1">Q<?php print "$post_counter"; ?>
+                                </span>
+                                <p><?php the_title(); ?></p>
+                            </div>
+                            <div class="rowLine flex">
+                                <span class=" circle_mark2">A</span>
+                                <p><?php the_field('answer'); ?></p>
+                            </div>
+                        </div>
+                        <?php $post_counter++; ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
         </div>
         </section>
     </div>
