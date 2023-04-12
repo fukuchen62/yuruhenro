34710cede8d8589bb7e288a865c12bd76984d819
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-13 02:12:54 +0900

    Update single-temple.php
    alt修正
    
    中西

--- single-temple.php
+++ single-temple.php
@@ -18,7 +18,7 @@
 
             <!-- お寺メイン画像 -->
             <div class="temple_img">
-                <img src="<?php the_field('t_eyecatch') ?>" alt="お寺の写真(霊山寺)" />
+                <img src="<?php the_field('t_eyecatch') ?>" alt="お寺の写真" />
             </div>
         </section>
 
@@ -30,13 +30,13 @@
                 <!-- おすすめ画像 -->
                 <ul class="slider2">
                     <li>
-                        <img src="<?php the_field('t_pic1') ?>" alt=" 1" />
+                        <img src="<?php the_field('t_pic1') ?>" alt="寺社の風景1" />
                     </li>
                     <li>
-                        <img src="<?php the_field('t_pic2') ?>" alt="2" />
+                        <img src="<?php the_field('t_pic2') ?>" alt="寺社の風景2" />
                     </li>
                     <li>
-                        <img src="<?php the_field('t_pic3') ?>" alt="3" />
+                        <img src="<?php the_field('t_pic3') ?>" alt="寺社の風景3" />
                     </li>
                 </ul>
 
