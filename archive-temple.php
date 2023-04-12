435408847d1214f0b247d828f1b72c605c36dfbf
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-12 20:54:33 +0900

    Update archive-temple.php
    概要編集
    
    中西

--- archive-temple.php
+++ archive-temple.php
@@ -6,10 +6,10 @@
         <section class="sec_mb40">
             <div>
                 <p class="big_cap">
-                        本サイト「＃ゆるへんろ」で巡る徳島二十三ヶ所のお寺の一覧ページです。
-                        クリックすると各お寺の詳細ページに飛びます。<br>
-                        気になるお寺をブックマークすると<a href="../bookmark" class="link">マイブックマーク</a>ページに登録出来ます。
-                    </p>
+                    本サイト「＃ゆるへんろ」で巡る徳島二十三ヶ所のお寺の一覧ページです。
+                    クリックすると各お寺の詳細ページに飛びます。<br>
+                    気になるお寺をブックマーク<i class="fa-regular fa-bookmark"></i>すると<a href="../bookmark" class="link">マイブックマーク</a>ページに登録出来ます。
+                </p>
             </div>
         </section>
         <section class="contents_width mb_100">
@@ -18,44 +18,44 @@
             <div class="shop_info_list flex">
                 <?php
                 $temple_args = array(
-                'post_type' => 'temple', // 投稿タイプ名
-                'posts_per_page' => -1, // 表示件数
-                'order' => 'ASC', // ランダム表示
+                    'post_type' => 'temple', // 投稿タイプ名
+                    'posts_per_page' => -1, // 表示件数
+                    'order' => 'ASC', // ランダム表示
                 );
                 $temple_query = new WP_Query($temple_args);
                 if ($temple_query->have_posts()) :
-                while ($temple_query->have_posts()) : $temple_query->the_post(); ?>
-                <a href="<?php the_permalink(); ?>">
-                    <div class="shop_info_card">
-                        <!-- 寺社カードのデザイン -->
-                        <div class="shop_info_caption">
-                            <img src="<?php the_field('t_eyecatch'); ?>" alt="施設の画像" />
-                        </div>
-                        <!-- 寺社名 -->
-                        <div class="shop_info_title">
-                            <p><?php the_field('t_numbername'); ?></p>
-                        </div>
-                        <!-- 周辺施設詳細 -->
-                        <div class="shop_info_text">
-                            <p>駐車場：<?php the_field('t_parking'); ?></p>
-                            <p>TEL：<?php the_field('t_tell'); ?></p>
-                            <p>公式HP：<?php the_field('t_url'); ?></p>
-                            <p>公式HP：
-                                            <?php if (get_field('t_url') != '-') {
-                                                echo 'あり';
-                                            } else {
-                                                echo 'なし';
-                                            } ?>
-                                        </p>
-                        </div>
-                        <!-- お気に入りボタン -->
-                        <div>
-                            <?php echo do_shortcode('[favorite_button post_id="" site_id=""]'); ?>
-                        </div>
-                    </div>
-                </a>
-                <?php endwhile ?>
-                <?php endif;?>
+                    while ($temple_query->have_posts()) : $temple_query->the_post(); ?>
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="shop_info_card">
+                                <!-- 寺社カードのデザイン -->
+                                <div class="shop_info_caption">
+                                    <img src="<?php the_field('t_eyecatch'); ?>" alt="施設の画像" />
+                                </div>
+                                <!-- 寺社名 -->
+                                <div class="shop_info_title">
+                                    <p><?php the_field('t_numbername'); ?></p>
+                                </div>
+                                <!-- 周辺施設詳細 -->
+                                <div class="shop_info_text">
+                                    <p>駐車場：<?php the_field('t_parking'); ?></p>
+                                    <p>TEL：<?php the_field('t_tell'); ?></p>
+                                    <p>公式HP：<?php the_field('t_url'); ?></p>
+                                    <p>公式HP：
+                                        <?php if (get_field('t_url') != '-') {
+                                            echo 'あり';
+                                        } else {
+                                            echo 'なし';
+                                        } ?>
+                                    </p>
+                                </div>
+                                <!-- お気に入りボタン -->
+                                <div>
+                                    <?php echo do_shortcode('[favorite_button post_id="" site_id=""]'); ?>
+                                </div>
+                            </div>
+                        </a>
+                    <?php endwhile ?>
+                <?php endif; ?>
             </div>
         </section>
     </article>
