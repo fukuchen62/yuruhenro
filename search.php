8346eb60434e90f0be823a806b040eac9c964fd5
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-13 19:41:10 +0900

    Update search.php
    周辺施設の検索テスト中
    
    中西

--- search.php
+++ search.php
@@ -52,7 +52,7 @@
 
 
         <section class="contents_width mb_100">
-            <h2 class="h3_bg mb_40">条件検索結果(<?php echo $count?>件)</h2>
+            <h2 class="h3_bg mb_40">寺社の条件検索結果(<?php echo $count?>件)</h2>
 
             <!-- 寺社結果一覧 -->
             <!-- <div class="searchresult_inner"> -->
@@ -77,6 +77,103 @@
                             <p>TEL：<?php the_field('t_tell'); ?></p>
                             <p>公式HP：
                                         <?php if (get_field('t_url') != '-') {
+                                            echo 'あり';
+                                        } else {
+                                            echo 'なし';
+                                        } ?>
+                                    </p>
+                            <?php echo do_shortcode('[favorite_button post_id="" site_id=""]'); ?>
+
+                        </div>
+                    </div>
+
+                </a>
+
+                <?php wp_reset_postdata(); ?>
+
+                <?php endwhile; ?>
+                <?php else : ?>
+                <div>
+                    <p>条件に合う検索結果はありませんでした。</p>
+                </div>
+                <?php endif; ?>
+                <!--カードここまで-->
+
+            </div>
+        </section>
+
+
+
+        <!-- 周辺施設の検索結果表示 -->
+
+
+        <?php
+        $area_slug = get_query_var('area');
+        $search_slug = get_query_var('searchh');
+
+        // クエリ作成
+        $args = [
+            'post_type' => 'facility',             //農業体験を検索
+            'posts_per_page' => -1,             //全件表示
+            'orderby' => 'rand',                //ランダムで表示する
+        ];
+        $taxquerysp = ['relation' => 'AND'];    // area，event_type，season，vege_type，target をAND検索
+
+        // 選択していない場合も考慮して条件で絞り込む。
+        if (!empty($area_slug)) {
+            $taxquerysp[] = [
+                'taxonomy' => 'area',           //タクソノミー：『エリア』
+                'terms' => $area_slug,          //スラッグ名
+                'field' => 'slug',              //スラッグ指定
+                'operator' => 'IN'
+            ];
+        }
+
+        // 選択していない場合も考慮して条件で絞り込む。
+        if (!empty($search_slug)) {
+            $taxquerysp[] = [
+                'taxonomy' => 'searchh',           //タクソノミー：『寺院内設備』
+                'terms' => $search_slug,          //スラッグ名
+                'field' => 'slug',              //スラッグ指定
+                'operator' => 'AND'
+            ];
+        }
+
+        $args['tax_query'] = $taxquerysp;       // 絞り込んだ情報を $args に代入する。
+        $the_query = new WP_Query($args);       // wpループ（$the_query）作成する。
+        $count = $the_query->post_count; //検索結果件数表示
+
+        ?>
+
+
+
+
+
+        <section class="contents_width mb_100">
+            <h2 class="h3_bg mb_40">周辺施設の条件検索結果(<?php echo $count?>件)</h2>
+
+            <!-- 寺社結果一覧 -->
+            <!-- <div class="searchresult_inner"> -->
+            <div class="searchresult_content">
+                <!--カードここから-->
+
+                <!-- 記事を表示するループ -->
+                <?php if ($the_query->have_posts()) : ?>
+                <?php while ($the_query->have_posts()) : ?>
+                <?php $the_query->the_post(); ?>
+                <a href="<?php the_permalink(); ?>">
+                    <div class="searchresultList">
+                        <div class="searchresultList_thumb">
+                            <img src="<?php the_field('a_thumbnail'); ?>" alt="寺社のアイキャッチ画像" />
+                        </div>
+                        <div class="searchresultList_data">
+                            <h3 class="h4_bg text_c"><?php the_field('f_name'); ?></h3>
+                            <!-- <p>拝観時間：10時～17時</p> -->
+                            <!-- <p>定休日：火曜日</p> -->
+                            <p>駐車場：<?php the_field('t_parking'); ?></p>
+                            <p>TEL：<?php the_field('t_tell'); ?></p>
+                            <p>公式HP：
+                                        <?php if (get_field('f_url') != '-') {
                                             echo 'あり';
                                         } else {
                                             echo 'なし';
