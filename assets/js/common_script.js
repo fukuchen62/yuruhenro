ec43eec278766e4bce064808ededead3eba36d6e
yuruhenro <128557320+yuruhenro@users.noreply.github.com>
2023-04-13 15:48:15 +0900

    common_script.js微修正
    slickにslideToScroll:4を追加しました。
    
    藤

--- assets/js/common_script.js
+++ assets/js/common_script.js
@@ -157,6 +157,10 @@ $(function () {
 
     $(".thumbnail").slick({
         slidesToShow: 4,
+        slideToScroll: 4,
+        // swipe: true,
+        // swipeToSlide: true,
+        // touchMove: true,
         asNavFor: ".slider",
         focusOnSelect: true,
         prevArrow: false,
