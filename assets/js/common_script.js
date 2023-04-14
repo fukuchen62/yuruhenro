29fc9f07317fdebb7bd35e10028937980b25dcc5
Shinomiya-Aki <shinomiya.aki.22@gmail.com>
2023-04-14 18:29:37 +0900

    headerの出現スピードを変更しました
    headerの出現スピードを変更しました
    
    四宮

--- assets/js/common_script.js
+++ assets/js/common_script.js
@@ -10,16 +10,12 @@ $(function () {
     var pagetop = $(".home header");
     pagetop.hide();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 700) {
+        if ($(this).scrollTop() > 150) {
             pagetop.fadeIn();
         } else {
             pagetop.fadeOut();
         }
     });
-    // pagetop.click(function () {
-    //     $("body, html").animate({ scrollTop: 0 }, 500);
-    //     return false;
-    // });
 });
 
 //--------------------------------------------------//
@@ -195,52 +191,59 @@ $(function () {
     });
 });
 
-
 /*===========================================================*/
 /* テキストが光りながら出現*/
 /*===========================================================*/
 
 function GlowAnimeControl() {
-  $('.glowAnime').each(function () {
-    var elemPos = $(this).offset().top - 50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight) {
-      $(this).addClass("glow");
-
-    } else {
-      $(this).removeClass("glow");
-    }
-  });
+    $(".glowAnime").each(function () {
+        var elemPos = $(this).offset().top - 50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass("glow");
+        } else {
+            $(this).removeClass("glow");
+        }
+    });
 }
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
-  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面をスクロールをしたら動かしたい場合の記述
+    GlowAnimeControl(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
-  //spanタグを追加する
+$(window).on("load", function () {
+    //spanタグを追加する
     var element = $(".glowAnime");
-  element.each(function () {
-    var text = $(this).text();
-    var textbox = "";
-    text.split('').forEach(function (t, i) {
-      if (t !== " ") {
-        if (i < 10) {
-          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
-        } else {
-        var n = i / 10;
-          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
-        }
-
-      } else {
-        textbox += t;
-      }
+    element.each(function () {
+        var text = $(this).text();
+        var textbox = "";
+        text.split("").forEach(function (t, i) {
+            if (t !== " ") {
+                if (i < 10) {
+                    textbox +=
+                        '<span style="animation-delay:.' +
+                        i +
+                        's;">' +
+                        t +
+                        "</span>";
+                } else {
+                    var n = i / 10;
+                    textbox +=
+                        '<span style="animation-delay:' +
+                        n +
+                        's;">' +
+                        t +
+                        "</span>";
+                }
+            } else {
+                textbox += t;
+            }
+        });
+        $(this).html(textbox);
     });
-    $(this).html(textbox);
-  });
 
-  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+    GlowAnimeControl(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
