66c796698d9c751930093e796892e7b1ab5cd2dd
nakanishikenta14 <127360555+nakanishikenta14@users.noreply.github.com>
2023-04-17 18:32:45 +0900

    キャッチフレーズ修正
    最初に表示されてた問題を修正しました
    
    中西

--- assets/js/common_script.js
+++ assets/js/common_script.js
@@ -197,53 +197,50 @@ $(function () {
 
 function GlowAnimeControl() {
     $(".glowAnime").each(function () {
-        var elemPos = $(this).offset().top - 50;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll >= elemPos - windowHeight) {
-            $(this).addClass("glow");
-        } else {
-            $(this).removeClass("glow");
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+        $(this).css("display", "block"); // ここで表示に変更
+        $(this).addClass("glow");
+    } else {
+        $(this).removeClass("glow");
         }
     });
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
-    GlowAnimeControl(); /* アニメーション用の関数を呼ぶ*/
-}); // ここまで画面をスクロールをしたら動かしたい場合の記述
+    GlowAnimeControl();
+});
 
-// 画面が読み込まれたらすぐに動かしたい場合の記述
 $(window).on("load", function () {
-    //spanタグを追加する
     var element = $(".glowAnime");
     element.each(function () {
-        var text = $(this).text();
-        var textbox = "";
+    var text = $(this).text();
+    var textbox = "";
         text.split("").forEach(function (t, i) {
-            if (t !== " ") {
-                if (i < 10) {
-                    textbox +=
-                        '<span style="animation-delay:.' +
-                        i +
-                        's;">' +
-                        t +
-                        "</span>";
-                } else {
-                    var n = i / 10;
-                    textbox +=
-                        '<span style="animation-delay:' +
-                        n +
-                        's;">' +
-                        t +
-                        "</span>";
-                }
+        if (t !== " ") {
+            if (i < 10) {
+                textbox +=
+                    '<span style="animation-delay:.'
+                    + i +
+            's;">' +
+            t +
+                    "</span>";
             } else {
-                textbox += t;
+                var n = i / 10;
+                textbox +=
+                    '<span style="animation-delay:' +
+                n +
+                's;">' +
+                    t +
+                    "</span>";
+            }
+        } else {
+            textbox += t;
             }
         });
         $(this).html(textbox);
     });
-
-    GlowAnimeControl(); /* アニメーション用の関数を呼ぶ*/
-}); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+    GlowAnimeControl();
+});
